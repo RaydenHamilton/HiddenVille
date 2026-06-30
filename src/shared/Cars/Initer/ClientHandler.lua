6266ad10d42b964fb5c235e0d47a193e9a892c74
Rayden <ragham25@gmail.com>
2026-06-30 04:17:58 -0400

    made less momery leaks with the cars

--- src/shared/Cars/Initer/ClientHandler.lua
+++ src/shared/Cars/Initer/ClientHandler.lua
@@ -5,18 +5,19 @@ local CameraClient = require(ReplicatedStorage.Shared.Cars.Camera.CameraClient)
 local DriveClient = require(ReplicatedStorage.Shared.Cars.DriveClient)
 local GaugesClient = require(ReplicatedStorage.Shared.Cars.GaugesClient)
 local LightClient = require(ReplicatedStorage.Shared.Cars.Lights.LightClient)
-local IgnitionClient = require(ReplicatedStorage.Shared.Cars.SoundHanlder.IgnitionClient)
 local SoundClient = require(ReplicatedStorage.Shared.Cars.SoundHanlder.SoundClient)
 
 local clientInit = {}
 
 function clientInit.init()
 	Workspace.CivCars.ChildAdded:Connect(function(CarModel: Model)
-		IgnitionClient.new(CarModel)
+		local seat = CarModel:WaitForChild("DriveSeat") :: Part
+		seat.Anchored = true
 	end)
 
 	Players.LocalPlayer.PlayerGui.ChildAdded:Connect(function(a0: Instance)
 		if a0.Name == "A-Chassis Interface" then
+			print("ui")
 			GaugesClient.new()
 			SoundClient.new()
 			CameraClient.new()
